ea232950027ebcd2e72648e8b6284c785a1489d6
Nghiep <me@nghiep.dev>
2024-04-18 19:08:42 +0700

    Clean repository

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,23 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        desktop: '1440px',
+      },
+      colors: {
+        primary: '#C20122',
+        secondary: '#A7011E',
+        tertiary: '#FFF68E',
+      },
+      dropShadow: {
+        normal: '0 4px 4px rgba(0, 0, 0, 0.25)',
       },
     },
   },
